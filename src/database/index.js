4b6c9937754948394dee8fb7615d840ea3b68269
nsolanki785 <solankinikhil802@gmail.com>
2025-07-09 00:25:00 +0530

    updtaed changes

--- src/database/index.js
+++ src/database/index.js
@@ -2,14 +2,18 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,     
-  process.env.DB_USER,     
-  process.env.DB_PASS,     
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',      
-  }
-);
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  port: 4032, // or custom port
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: true
+    }
+  },
+  logging: false
+});
+
 
 module.exports = sequelize;
